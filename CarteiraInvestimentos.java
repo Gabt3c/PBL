67b79ccc8abca87255ec96d49ca65bd804d6bce0
Gabt3c <gking3190@gmail.com>
2026-09-19 00:41:26 -0300

    checkpoint CarteiraInvestimentos

--- CarteiraInvestimentos.java
+++ CarteiraInvestimentos.java
@@ -1,3 +1,8 @@
-public class CarteiraInvestimentos extends Cliente {
-    
+public class CarteiraInvestimentos{
+        private Posicao posicao;
+        private Cliente cliente;
+        public CarteiraInvestimentos(String nomePosicao, String nomeCliente){
+            this.posicao = new Posicao(nomePosicao);
+        }
+
 }
