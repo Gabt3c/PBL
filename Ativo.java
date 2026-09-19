31704fba94f494ea76efa11d18d838e70e42bb09
Gabt3c <gking3190@gmail.com>
2026-09-18 22:47:10 -0300

    feat: adição dos setters

--- Ativo.java
+++ Ativo.java
@@ -2,4 +2,15 @@ public class Ativo {
     private int codigo;
     private String nome;
     private double precoAtual;
+
+    //setters
+    public void setCodigo(int codigo){
+        this.codigo=codigo;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public void setPrecoAtual(double precoAtual){
+        this.precoAtual=precoAtual;
+    }
 }
