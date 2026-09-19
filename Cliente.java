252a9c168098f27f9593704cdad6dcadefda9d6d
jonh-victor <joao.victor.jvvbadaro@gmail.com>
2026-09-18 23:33:56 -0300

    feature: add constructor

--- Cliente.java
+++ Cliente.java
@@ -1,7 +1,10 @@
 public class Cliente {
     private String nome;
     private String CPF;
-
+    public Cliente (String nome, String CPF){
+        this.nome = nome;
+        this.CPF = CPF;
+    }
     setNome(String nome){
         this.nome = nome;
     }
