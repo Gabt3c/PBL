ad352cacda5640542c16ccbd1a301d1f314ed4f2
jonh-victor <joao.victor.jvvbadaro@gmail.com>
2026-09-20 16:12:51 -0300

    checkpoint para resolução de problema

--- CarteiraInvestimentos.java
+++ CarteiraInvestimentos.java
@@ -1,19 +1,18 @@
 public class CarteiraInvestimentos{
         public Posicao[] posicao;
         public Cliente cliente;
-        public int qtAtivo;
 
-        public CarteiraInvestimentos(int qtAtivo){
-            this.posicao = new Posicao[qtAtivo];
+        public CarteiraInvestimentos(){
+            this.posicao = new Posicao[cliente.qtativo];
         }
 
         public void adicionarPosicao(Ativo ativo, int quantidade){
-            posicao[this.qtAtivo].calcularValor();
+            posicao[cliente.qtativo].calcularValor();
         }
         
         public double calcularPatrimonio(){
             double patrimonioTotal = 0;
-            for (int i = 0; this.qtAtivo > i; i++){
+            for (int i = 0; cliente.qtativo > i; i++){
                 patrimonioTotal = patrimonioTotal + posicao[i].calcularValor();
             }
             return patrimonioTotal;
@@ -22,7 +21,7 @@ public class CarteiraInvestimentos{
         public void exibirResumo(){
             System.out.println("Ativo  Preço atual  Quantidade");
             for (int i = 0; i < posicao.length; i++){
-                System.out.println(posicao[this.qtAtivo].getNomeAtivo()/*  + "  " + Posicao[]*/);
+                System.out.println(posicao[i].getNomeAtivo()/*  + "  " + Posicao[]*/);
             }
             
         }
--- Cliente.java
+++ Cliente.java
@@ -1,17 +1,20 @@
 public class Cliente {
     private String nome;
     private String CPF;
+    public int qtativo;
     //Composições
     protected Posicao[] posicao;
     protected Ativo[] ativo;
     protected CarteiraInvestimentos ctInvestimento;
     //Construtor
     public Cliente (String nome, String CPF, int qtAtivo){
+        this.qtativo = qtAtivo;
         this.nome = nome;
         this.CPF = CPF;
         this.posicao = new Posicao[qtAtivo];
         this.ativo = new Ativo[qtAtivo];
-        this.ctInvestimento = new CarteiraInvestimentos(null, qtAtivo);
+
+        this.ctInvestimento = new CarteiraInvestimentos();
     }
     //Setters e Getters
     public void setNome(String nome){
@@ -26,4 +29,8 @@ public class Cliente {
     public String getCPF(){
         return this.CPF;
     }
+    /*        public int getQtAtivo(){
+        return this.qtativo; 
+
+}*/
 }
